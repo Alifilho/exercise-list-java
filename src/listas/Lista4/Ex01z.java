7a879c6c8d542748139f0c92767315edca493861
LucasFernandes0101 <53587543+LucasFernandes0101@users.noreply.github.com>
2019-08-12 20:37:36 -0300

    Update Ex01z.java

--- src/listas/Lista4/Ex01z.java
+++ src/listas/Lista4/Ex01z.java
@@ -1,6 +1,26 @@
 package listas.Lista4;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex01z {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Digite algo: ");
+        String var = sc.next();
+        
+        int a = var.length();
+        
+        String ast = "-";
+        
+        for (int i = 0; i <= a; i++) {
+            ast += "-";
+        }
+        
+        System.out.println("*" + ast + "*");
+        System.out.println("* " + var + " *");
+        System.out.println("*" + ast + "*");
     }
 }
