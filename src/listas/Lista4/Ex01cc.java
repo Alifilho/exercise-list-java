9ba9c954ec4e95ccd6e68b4a1b11e783716d02c1
Jott4 <53587274+Jott4@users.noreply.github.com>
2019-08-13 18:46:57 -0300

    Update Ex01cc.java

--- src/listas/Lista4/Ex01cc.java
+++ src/listas/Lista4/Ex01cc.java
@@ -1,6 +1,18 @@
 package listas.Lista4;
 public class Ex01cc {
+    
+        public static int fibo(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
     public static void main(String[] args) {
-        
+              
+        int i = 0; // numero que começa a sequencia de fibonnaci
+        for (;; i++) {
+            System.out.print(fibo(i) + " ");
+            
     }
 }
