8395339b16b69f05e590dedcdb7c8557fa68bd47
LucasFernandes0101 <53587543+LucasFernandes0101@users.noreply.github.com>
2019-08-13 19:23:51 -0300

    Update Ex02j.java

--- src/listas/Lista4/Ex02j.java
+++ src/listas/Lista4/Ex02j.java
@@ -1,6 +1,27 @@
 package listas.Lista4;
 public class Ex02j {
-    public static void main(String[] args) {
+    public static int[] func(int x){
+        int[] vet = new int[x+1];
         
+        for (int i = 0; i <= x; i++) {
+            vet[i] = i;
+        }
+        
+        return vet;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o valor de X: ");
+        int x = sc.nextInt();
+        
+        int[] vet = new int[x+1];
+        
+        vet = func(x);
+        
+        System.out.println(Arrays.toString(vet));
     }
 }
+        
+    
+
